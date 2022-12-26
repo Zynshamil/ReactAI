a85715dbf97399b8cc218036de2af573fa5447eb
Zyn Shamil <shamil3637@gmail.com>
2022-12-26 15:21:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import OptionSelection from './component/OptionSelection';
+import { arrayItems } from './AIOptions';
+
+function App() {
+  return (
+    <div className="App">
+   <OptionSelection/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/OptionSelection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OptionSelection() {
+  return (
+    <div className='heading'>React AI</div>
+  )
+}
+
+export default OptionSelection
